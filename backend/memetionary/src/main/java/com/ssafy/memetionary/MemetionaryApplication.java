ba33f2405f8535df27c8515e45821194b2b2c602
박현준 <qkrguswns25@naver.com>
2023-10-16 14:14:22 +0900

    init

---
+++ backend/memetionary/src/main/java/com/ssafy/memetionary/MemetionaryApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.memetionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemetionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemetionaryApplication.class, args);
+	}
+
+}
