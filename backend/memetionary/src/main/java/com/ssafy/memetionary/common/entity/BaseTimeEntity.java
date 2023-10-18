10362fe6e54755d964a215c145b92101a019c1c0
박현준 <qkrguswns25@naver.com>
2023-10-18 10:59:39 +0900

    feat: 소셜 회원 가입 및 로그인

---
+++ backend/memetionary/src/main/java/com/ssafy/memetionary/common/entity/BaseTimeEntity.java
@@ -0,0 +1,15 @@
+package com.ssafy.memetionary.common.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseTimeEntity extends BaseCreateTimeEntity {
+    @LastModifiedDate
+    private LocalDateTime updateDate;
+}
