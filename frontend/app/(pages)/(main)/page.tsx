1bd0d0826104c0f3446ecbe8286a5e5f665901be
joung <15gpfk@naver.com>
2023-10-19 14:12:29 +0900

    feat: 프리텐다드 폰트 적용

--- frontend/app/(pages)/(main)/page.tsx
+++ frontend/app/(pages)/(main)/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
@@ -14,30 +13,10 @@ export default function Home() {
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          ></a>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className={styles.center}></div>
 
       <div className={styles.grid}>
         <a
