1cdbc64ce9eae565920ae0e56ec647cc7d8f7c99
joung <15gpfk@naver.com>
2023-10-19 13:52:19 +0900

    feat: FE폴더 생성

---
+++ frontend/app/utils/recoilRootProvider.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import { RecoilRoot } from 'recoil'
+const RecoilRootProvider = ({ children }: { children: React.ReactNode }) => {
+  return <RecoilRoot>{children}</RecoilRoot>
+}
+
+export default RecoilRootProvider
