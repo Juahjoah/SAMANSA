57969669b6a1a2ff03b708d5ad1e2197eff70038
hwanhee <dup07168@naver.com>
2023-10-19 14:57:04 +0900

    repository: 자동정렬 및 반환 값 변경 (#10)

--- backend/memetionary/src/main/java/com/ssafy/memetionary/word/service/WordService.java
+++ backend/memetionary/src/main/java/com/ssafy/memetionary/word/service/WordService.java
@@ -17,20 +17,22 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Transactional(readOnly = true)
 public class WordService {
+
     private final WordRepository wordRepository;
     private final MemberRepository memberRepository;
 
     @Transactional
-    public void save(WordRegisterDto wordRegisterDto, String memberId) {
-      Member member = memberRepository.findById(memberId).get();
-      //해시태그 분리
-      //해시태그 모두 돌면서 저장할거 저장하기
-      Word word = Word.builder()
-          .wordName(wordRegisterDto.getWordName())
-          .wordDescription(wordRegisterDto.getWordDescription())
-          .wordExample(wordRegisterDto.getWordExample())
-          .member(member)
-          .build();
-      wordRepository.save(word);
+    public Word save(WordRegisterDto wordRegisterDto, String memberId) {
+        Member member = memberRepository.findById(memberId).get();
+        //해시태그 분리
+        //해시태그 모두 돌면서 저장할거 저장하기
+        Word word = Word.builder()
+            .wordName(wordRegisterDto.getWordName())
+            .wordDescription(wordRegisterDto.getWordDescription())
+            .wordExample(wordRegisterDto.getWordExample())
+            .member(member)
+            .build();
+        wordRepository.save(word);
+        return word;
     }
 }
