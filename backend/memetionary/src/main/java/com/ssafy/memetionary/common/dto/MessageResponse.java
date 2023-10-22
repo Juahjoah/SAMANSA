59a1d6f736a4b47f02e06bfc1458c1cb37e69cf1
H4R1B0 <qkrguswns25@naver.com>
2023-10-22 18:15:19 +0900

    feat: 사용자 존재 예외처리 (#13)

--- backend/memetionary/src/main/java/com/ssafy/memetionary/common/dto/MessageResponse.java
+++ backend/memetionary/src/main/java/com/ssafy/memetionary/common/dto/MessageResponse.java
@@ -1,14 +1,10 @@
 package com.ssafy.memetionary.common.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data @Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@Builder
 public class MessageResponse {
+
     private String message;
 
 }
