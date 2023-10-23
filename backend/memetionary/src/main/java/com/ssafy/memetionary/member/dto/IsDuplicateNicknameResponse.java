1978c2177d40b9671c9772c195bb0117d2b89207
박현준 <qkrguswns25@naver.com>
2023-10-23 17:42:00 +0900

    fix: 닉네임 중복 검사 오류 수정 (#7)

--- backend/memetionary/src/main/java/com/ssafy/memetionary/member/dto/IsDuplicateNicknameResponse.java
+++ backend/memetionary/src/main/java/com/ssafy/memetionary/member/dto/IsDuplicateNicknameResponse.java
@@ -1,8 +1,10 @@
 package com.ssafy.memetionary.member.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class IsDuplicateNicknameResponse {
 
     private boolean isDuplicate;
