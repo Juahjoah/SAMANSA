e8dca5bf1d5bbfa5f4b7ee71ff55cface69b7789
박현준 <qkrguswns25@naver.com>
2023-10-24 09:27:08 +0900

    feat: 예외처리 적용

---
+++ backend/memetionary/src/main/java/com/ssafy/memetionary/common/CustomErrorType.java
@@ -0,0 +1,14 @@
+package com.ssafy.memetionary.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CustomErrorType {
+
+    MEMBER_NOT_FOUND("사용자를 찾을 수 없습니다.");
+
+    private final String message;
+
+}
