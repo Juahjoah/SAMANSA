0117a7cf1f5d3e48af992dacd904afc5d7bc16b7
김주아 <07juahlily21@naver.com>
2023-10-24 17:34:06 +0900

    fix: 프론트 빌드 에러 해결 - 일단 로그인 코드 주석처리

--- frontend/src/app/auth/redirect/page.tsx
+++ frontend/src/app/auth/redirect/page.tsx
@@ -1,54 +1,54 @@
 'use client';
 
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 // import { usePathname, useRouter } from 'next/navigation';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 
 export default function RedirectPage() {
-  const accessToken = new URL(window.location.href).searchParams.get('token');
-  const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+  // const accessToken = new URL(window.location.href).searchParams.get('token');
+  // const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
-  const router = useRouter();
-  // const pathname = usePathname();
+  // const router = useRouter();
+  // // const pathname = usePathname();
 
-  useEffect(() => {
-    // 토큰이 있는지 확인
-    if (accessToken) {
-      // 토큰을 세션 스토리지에 저장
-      sessionStorage.setItem('accessToken', accessToken);
-      // console.log("토큰 저장 완료", accessToken);
+  // useEffect(() => {
+  //   // 토큰이 있는지 확인
+  //   if (accessToken) {
+  //     // 토큰을 세션 스토리지에 저장
+  //     sessionStorage.setItem('accessToken', accessToken);
+  //     // console.log("토큰 저장 완료", accessToken);
 
-      // 최초 로그인 여부 확인
-      fetch(`${BASE_URL}/member/nickname/change`, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-        .then((response) => response.json())
-        .then((userData) => {
-          console.log('사용자 정보 요청 성공:', userData);
-          const isFirstLogin = !userData.isChange;
+  //     // 최초 로그인 여부 확인
+  //     fetch(`${BASE_URL}/member/nickname/change`, {
+  //       method: 'GET',
+  //       headers: {
+  //         Authorization: `Bearer ${accessToken}`,
+  //       },
+  //     })
+  //       .then((response) => response.json())
+  //       .then((userData) => {
+  //         console.log('사용자 정보 요청 성공:', userData);
+  //         const isFirstLogin = !userData.isChange;
 
-          if (isFirstLogin) {
-            router.push('/auth/nickname'); // 최초 로그인 사용자
-            console.log('첫 로그인');
-          } else {
-            console.log('기존 로그인');
-            const { nickname } = userData;
-            sessionStorage.setItem('nickname', nickname);
-            router.push('/main'); // 기존 로그인 사용자
-          }
-        })
-        .catch((error) => {
-          console.error('사용자 정보 요청 실패:', error);
-          router.push('/');
-        });
-    } else {
-      // 토큰이 없으면 메인 페이지로 이동
-      router.push('/');
-    }
-  }, [router, BASE_URL, accessToken]);
+  //         if (isFirstLogin) {
+  //           router.push('/auth/nickname'); // 최초 로그인 사용자
+  //           console.log('첫 로그인');
+  //         } else {
+  //           console.log('기존 로그인');
+  //           const { nickname } = userData;
+  //           sessionStorage.setItem('nickname', nickname);
+  //           router.push('/main'); // 기존 로그인 사용자
+  //         }
+  //       })
+  //       .catch((error) => {
+  //         console.error('사용자 정보 요청 실패:', error);
+  //         router.push('/');
+  //       });
+  //   } else {
+  //     // 토큰이 없으면 메인 페이지로 이동
+  //     router.push('/');
+  //   }
+  // }, [router, BASE_URL, accessToken]);
 
   return (
     <div>
