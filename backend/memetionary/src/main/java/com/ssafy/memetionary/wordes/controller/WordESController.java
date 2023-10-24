fd0a63d00433423cac47f467e51c5b2a0bc2e234
hwanhee <dup07168@naver.com>
2023-10-24 15:29:04 +0900

    feat: 단어 삭제 기능 추가(#17)

--- backend/memetionary/src/main/java/com/ssafy/memetionary/wordes/controller/WordESController.java
+++ backend/memetionary/src/main/java/com/ssafy/memetionary/wordes/controller/WordESController.java
@@ -1,15 +1,23 @@
 package com.ssafy.memetionary.wordes.controller;
 
+import com.ssafy.memetionary.common.CustomErrorType;
 import com.ssafy.memetionary.common.dto.MessageResponse;
+import com.ssafy.memetionary.common.exception.WordNotFoundException;
 import com.ssafy.memetionary.util.HeaderUtils;
+import com.ssafy.memetionary.word.entity.Word;
+import com.ssafy.memetionary.wordes.document.WordES;
 import com.ssafy.memetionary.wordes.dto.WordESRegisterRequest;
+import com.ssafy.memetionary.wordes.repository.WordESRepository;
 import com.ssafy.memetionary.wordes.service.WordESService;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -22,8 +30,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WordESController {
 
     private final WordESService wordESService;
+    private final WordESRepository wordESRepository;
     private final HeaderUtils headerUtils;
 
+    //엘라스틱 서치 단어 등록 - 단어 4-1
     @PostMapping
     public ResponseEntity<MessageResponse> registerWord(
         @RequestBody WordESRegisterRequest request,
@@ -34,5 +44,25 @@ public class WordESController {
         return ResponseEntity.status(HttpStatus.OK)
             .body(MessageResponse.builder().message("단어 등록 성공").build());
     }
-    //엘라스틱 서치 단어 등록 - 단어 4-1
+
+    //엘라스틱 서치 단어 삭제 - 단어 7
+    @DeleteMapping("/{wordId}")
+    public ResponseEntity<MessageResponse> deleteWord(@PathVariable("wordId") String wordId,
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader
+    ){
+        String memberId = headerUtils.getMemberId(authorizationHeader);
+
+        WordES wordes = wordESRepository.findById(wordId).orElseThrow(() -> new WordNotFoundException(CustomErrorType.WORD_NOT_FOUND.getMessage()));
+
+        // 만약 작성자가 맞다면 진행
+        if(wordes.getMemberId().equals(memberId)){
+            wordESService.delete(wordes);
+        }else {// 만약 작성자가 아니라면 삭제 불가능
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(MessageResponse.builder().message("단어 삭제 실패 - 작성자가 아닙니다.").build());
+        }
+
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(MessageResponse.builder().message("단어 삭제 성공").build());
+    }
 }
