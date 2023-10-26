7d3e3c2f4b0483034a0f4db9c259df3c62ef6538
hwanhee <dup07168@naver.com>
2023-10-26 12:45:25 +0900

    feat: 단어 검색 응답 Dto 생성(#25)

---
+++ backend/memetionary/src/main/java/com/ssafy/memetionary/wordes/dto/WordESSearchResponse.java
@@ -0,0 +1,18 @@
+package com.ssafy.memetionary.wordes.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class WordESSearchResponse {
+    private String id;
+    private String wordName;
+    private String wordDescription;
+    private String wordExample;
+    private List<String> hashtagList;
+    private String memberNickname;
+    private LocalDate createDate;
+}
