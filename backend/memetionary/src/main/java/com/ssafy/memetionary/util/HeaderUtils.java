87cf3b029d0bd7e5386744ac820afc201b87de08
박현준 <qkrguswns25@naver.com>
2023-10-26 09:31:02 +0900

    feat: 사용자 IP 조회 (#18)

--- backend/memetionary/src/main/java/com/ssafy/memetionary/util/HeaderUtils.java
+++ backend/memetionary/src/main/java/com/ssafy/memetionary/util/HeaderUtils.java
@@ -1,6 +1,7 @@
 package com.ssafy.memetionary.util;
 
 import com.ssafy.memetionary.oauth2.token.JwtTokenService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,5 +15,11 @@ public class HeaderUtils {
     public String getMemberId(String authorizationHeader) {
         String accessToken = authorizationHeader.substring(7);
         return jwtTokenService.findMemberId(accessToken);
+    }
+
+    public String getClientIP(HttpServletRequest request) {
+        String clientIP1 = request.getRemoteAddr();
+        String clientIP2 = request.getHeader("X-Forwarded-For");
+        return clientIP2 != null ? clientIP2 : clientIP1;
     }
 }
