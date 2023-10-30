25af3eadb4355246dea7d95a7d897c9c364f2cb9
hwanhee <dup07168@naver.com>
2023-10-30 17:15:22 +0900

    refactor: 검색 paging 추가

--- backend/memetionary/src/main/java/com/ssafy/memetionary/wordes/repository/WordESRepository.java
+++ backend/memetionary/src/main/java/com/ssafy/memetionary/wordes/repository/WordESRepository.java
@@ -1,9 +1,14 @@
 package com.ssafy.memetionary.wordes.repository;
 
 import com.ssafy.memetionary.wordes.document.WordES;
+
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface WordESRepository extends ElasticsearchRepository<WordES, String>, WordESRepositoryCustom {
-    public List<WordES> findByName(String name);
+public interface WordESRepository extends ElasticsearchRepository<WordES, String>,
+    WordESRepositoryCustom {
+
+    public Page<WordES> findByName(String name, Pageable pageable);
 }
