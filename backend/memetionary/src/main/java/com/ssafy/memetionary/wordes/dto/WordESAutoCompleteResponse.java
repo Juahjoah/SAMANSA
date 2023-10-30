a2925de87ebc304b70e016780017f7b54177ae7b
박현준 <qkrguswns25@naver.com>
2023-10-30 17:14:13 +0900

    feat: 단어 자동완성 (#28)

---
+++ backend/memetionary/src/main/java/com/ssafy/memetionary/wordes/dto/WordESAutoCompleteResponse.java
@@ -0,0 +1,14 @@
+package com.ssafy.memetionary.wordes.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class WordESAutoCompleteResponse {
+
+    private List<String> words;
+
+}
