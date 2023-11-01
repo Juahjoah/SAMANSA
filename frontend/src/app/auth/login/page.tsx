100edcf572e771d8f763d02da9c3c88bf448ea2c
김주아 <07juahlily21@naver.com>
2023-11-01 14:43:44 +0900

    design: login 페이지 next.js image 불러오는 방식 변경 (#32)

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -4,12 +4,20 @@ import NaverLoginButton from '@/app/auth/login/_components/NaverLoginButton';
 import Logo from '@/public/assets/logo_b_samansa.png';
 
 import styles from './LoginPage.module.css';
+import Image from 'next/image';
 
 export default function LoginPage() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.login}>
-        <img src={Logo.src} width="80%" alt="logo" />
+        <Image
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: '80%', height: '100%' }}
+          src={Logo}
+          alt="logo"
+        />
         <h2 className={styles.loginTitle}> 사만사에 오신 것을 환영합니다. </h2>
         <p className={styles.loginMsg}> 간편하게 로그인 해보세요!</p>
         <div className={styles.loginBtn}>
