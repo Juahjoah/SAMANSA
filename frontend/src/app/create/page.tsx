39326c236560f60972e99976551b8dd45593fea8
석정원 <15gpfk@naver.com>
2023-11-01 21:22:08 +0900

    fix: 오류수정

--- frontend/src/app/create/page.tsx
+++ frontend/src/app/create/page.tsx
@@ -28,7 +28,13 @@ export default function CreatePage() {
       <div className={styles.regist}>
         <div className={styles.title}>밈 등록 페이지</div>
         <div className={styles.icon}>
-          <Image src={hamburgerIcon} loader={imageLoader} height={70} width={80} alt="햄버그바" />
+          <Image
+            src={hamburgerIcon}
+            loader={imageLoader}
+            height={70}
+            width={80}
+            alt="햄버그바"
+          />
         </div>
         <div className={styles.registcard}>
           <div className={styles.wordTag}>
