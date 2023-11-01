1baf05434e7a78e3dc8094a87bbf35d420b9f0b9
김주아 <07juahlily21@naver.com>
2023-11-01 14:57:34 +0900

    design: login 페이지 css 추가 (#32)

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,7 +1,8 @@
-// import NaverLoginButton from './_components/NaverLoginButton';
 import KakaoLoginButton from '@/app/auth/login/_components/KakaoLoginButton';
 import NaverLoginButton from '@/app/auth/login/_components/NaverLoginButton';
 import Logo from '@/public/assets/logo_b_samansa.png';
+
+// import { useRouter } from 'next/navigation';
 
 import styles from './LoginPage.module.css';
 import Image from 'next/image';
