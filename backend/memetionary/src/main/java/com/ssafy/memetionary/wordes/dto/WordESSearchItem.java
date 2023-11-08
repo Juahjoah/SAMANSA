7af9b9b088a5050659d487bfa77a10bf53e91809
hwanhee <dup07168@naver.com>
2023-11-08 13:57:52 +0900

    rename: likecount, dislikeCount의 C 대문자로 고침(#44)

--- backend/memetionary/src/main/java/com/ssafy/memetionary/wordes/dto/WordESSearchItem.java
+++ backend/memetionary/src/main/java/com/ssafy/memetionary/wordes/dto/WordESSearchItem.java
@@ -17,8 +17,8 @@ public class WordESSearchItem {
     private List<String> hashtagList;
     private String memberNickname;
     private LocalDateTime createDate;
-    private long likecount;
-    private long dislikecount;
+    private long likeCount;
+    private long dislikeCount;
     private boolean hasLike;
     private boolean hasDislike;
     private boolean isWriter;
