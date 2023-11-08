b22d19e5ec913112a0e544e784e8e0f286079a0a
김주아 <07juahlily21@naver.com>
2023-11-08 10:39:55 +0900

    design: 로그인 버튼 CSS 수정 (#40)

--- frontend/src/app/auth/login/_components/NaverLoginButton/index.tsx
+++ frontend/src/app/auth/login/_components/NaverLoginButton/index.tsx
@@ -37,7 +37,12 @@ export default function NaverLoginButton() {
         width={0}
         height={0}
         sizes="100vw"
-        style={{ width: '100%', height: 'auto' }}
+        // style={{ width: '100%', height: 'auto' }}
+        style={
+          isMobile
+            ? { width: '130%', height: 'auto' }
+            : { width: '100%', height: 'auto' }
+        }
       />
     </div>
   );
