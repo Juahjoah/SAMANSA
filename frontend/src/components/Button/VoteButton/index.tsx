568c1d545a94ffa3e0fb8203af20dd98633e7bcf
Jiyoung Park <parkninni@icloud.com>
2023-11-08 13:41:27 +0900

    feat:좋아요 버튼 api fetching logic 추가

--- frontend/src/components/Button/VoteButton/index.tsx
+++ frontend/src/components/Button/VoteButton/index.tsx
@@ -1,8 +1,25 @@
 'use client';
 import VoteButtonBase from './VoteButtonBase';
+import { useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+async function updateVoteCount(id: string, like: boolean) {
+  const response = await fetch(`${BASE_URL}/word/like`, {
+    method: 'PUT',
+    body: JSON.stringify({ wordId: id, wordLike: like }),
+  });
+  return response.json();
+}
 
 export default function VoteButton(props: VoteButtonProps) {
-  // TODO: api calls with hooks
+  const queryClient = useQueryClient();
+  const updateVoteCountMutation = useMutation({
+    mutationFn: updateVoteCount,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['votes'] });
+    },
+  });
   return (
     <VoteButtonBase
       onVoteDown={() => console.log('down')}
---
+++ frontend/src/utils/hydrateOnClient.tsx
@@ -0,0 +1,4 @@
+'use client';
+
+import { hydrate as HydrateOnClient } from '@tanstack/react-query';
+export default HydrateOnClient;
