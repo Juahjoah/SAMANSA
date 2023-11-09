50c50d08d3979a9550fbf4d67d95f9e256066611
석정원 <15gpfk@naver.com>
2023-11-09 14:11:36 +0900

    Update index.tsx

--- frontend/src/components/Button/DeleteButton/index.tsx
+++ frontend/src/components/Button/DeleteButton/index.tsx
@@ -3,6 +3,8 @@
 import styles from './DeleteButton.module.css';
 import { useState, useEffect } from 'react';
 
+import Modal from '@/components/Modal';
+
 type ButtonProps = {
   id?: string;
   memberNickname?: string;
@@ -32,30 +34,48 @@ export default function DeleteButton({
   const { id, memberNickname } = requestData; // 카드의 id, 유저닉네임
   const url = `${process.env.NEXT_PUBLIC_REDIRECT_URI}`;
 
-  // 로그인 유저의 닉네임 정보
-  const nickname: string | null =
-    typeof window !== 'undefined' ? sessionStorage.getItem('nickname') : null;
-
   const [mounted, setMounted] = useState<boolean>(false);
   useEffect(() => {
+    // 로그인 유저의 닉네임 정보
+    const nickname: string | null =
+      typeof window !== 'undefined' ? sessionStorage.getItem('nickname') : null;
+
     if (nickname === memberNickname) {
       setMounted(true);
     }
-  }, [nickname, memberNickname]);
+  }, []);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const ModalOpen = () => {
+    setIsModalOpen(true);
+  };
 
   const DeleteWord = async () => {
     DeleteData(`${url}/api/word/${id}`)
       .then((data) => {
         console.log(data);
+        window.location.href = url;
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div className={styles.base}>
-      {mounted && <div onClick={DeleteWord}>삭제</div>}
+      {!mounted && <div onClick={ModalOpen}>삭제</div>}
+      {isModalOpen && (
+        <Modal
+          visible={isModalOpen}
+          maskClosable={true}
+          onClose={closeModal}
+          action={DeleteWord}
+        />
+      )}
     </div>
   );
 }
