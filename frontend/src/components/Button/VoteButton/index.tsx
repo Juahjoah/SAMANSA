fc285d8bfc6a3b3f18196d278a0f844835e2940a
Ninni Park <ninni@NinnisMcBookAir.lan>
2023-11-14 00:44:34 +0900

    test: 좋아요 버튼 테스트

--- frontend/src/components/Button/VoteButton/index.tsx
+++ frontend/src/components/Button/VoteButton/index.tsx
@@ -10,7 +10,6 @@ async function updateVoteCount({ id, like }: UpdateVoteCountRequest) {
   const response = await fetch(`${BASE_URL}/word/like`, {
     headers: {
       'Content-Type': 'application/json',
-      'x-forwarded-for': '192.168.31.210',
     },
     method: 'PUT',
     body: JSON.stringify({ wordId: id, wordLike: like }),
@@ -40,68 +39,55 @@ export default function VoteButton({
   if (hasLike) initialVoteState = VoteState.UP;
   if (hasDislike) initialVoteState = VoteState.DOWN;
 
-  const [voteState, setVoteState] = useState(initialVoteState);
-  const [likeCountState, setLikeCountState] = useState(likeCount);
-  const [dislikeCountState, setDislikeCountState] = useState(dislikeCount);
+  const [voteState, setVoteState] = useState<VoteState>(initialVoteState);
+  const [upVotes, setUpVotes] = useState(likeCount);
+  const [downVotes, setDownVotes] = useState(dislikeCount);
 
-  // const handleLike = () => {
-  //   if (voteState === VoteState.UP) {
-  //     setVoteState(VoteState.NONE);
-  //     updateVoteCountMutation.mutate({ id: wordId, like: true });
-  //     return;
-  //   }
-  //   setVoteState(VoteState.UP);
-  //   updateVoteCountMutation.mutate({ id: wordId, like: true });
-  // };
-  // const handleDislike = () => {
-  //   if (voteState === VoteState.DOWN) {
-  //     setVoteState(VoteState.NONE);
-  //     updateVoteCountMutation.mutate({ id: wordId, like: false });
-  //     return;
-  //   }
-  //   setVoteState(VoteState.DOWN);
-  //   updateVoteCountMutation.mutate({ id: wordId, like: false });
-  // };
   const handleLike = () => {
+    // if it's already liked, initialLikeCount - 1
     if (voteState === VoteState.UP) {
-      // 이미 좋아요가 눌린 상태, 좋아요 취소
       setVoteState(VoteState.NONE);
-      setLikeCountState(likeCount - 1);
-    } else {
-      // 좋아요가 눌리지 않은 상태, 좋아요 적용
-      setVoteState(VoteState.UP);
-      setLikeCountState(likeCount + 1);
-      if (voteState === VoteState.DOWN) {
-        // 싫어요가 눌린 상태였다면, 싫어요 취소
-        setDislikeCountState(dislikeCount - 1);
-      }
+      setUpVotes(likeCount - 1);
+      updateVoteCountMutation.mutate({ id: wordId, like: true });
+      return;
     }
+    // newly like, set Dislike with original dislikeCount
+    setVoteState(VoteState.UP);
+    setUpVotes(upVotes + 1);
+    setDownVotes(dislikeCount);
     updateVoteCountMutation.mutate({ id: wordId, like: true });
-  };
-
-  const handleDislike = () => {
+    // if already disliked, remove the dislike and add the like
     if (voteState === VoteState.DOWN) {
-      // 이미 싫어요가 눌린 상태, 싫어요 취소
-      setVoteState(VoteState.NONE);
-      setDislikeCountState(dislikeCount - 1);
-    } else {
-      // 싫어요가 눌리지 않은 상태, 싫어요 적용
-      setVoteState(VoteState.DOWN);
-      setDislikeCountState(dislikeCount + 1);
-      if (voteState === VoteState.UP) {
-        // 좋아요가 눌린 상태였다면, 좋아요 취소
-        setLikeCountState(likeCount - 1);
-      }
+      setDownVotes(downVotes - 1);
+      setUpVotes(likeCount + 1);
     }
+  };
+  const handleDislike = () => {
+    // if it's already disliked, remove the dislike
+    if (voteState === VoteState.DOWN) {
+      setVoteState(VoteState.NONE);
+      setDownVotes(downVotes - 1);
+      updateVoteCountMutation.mutate({ id: wordId, like: false });
+      return;
+    }
+    // newly dislike, set Like with original likeCount
+    setVoteState(VoteState.DOWN);
+    setDownVotes(downVotes + 1);
+    setUpVotes(likeCount);
     updateVoteCountMutation.mutate({ id: wordId, like: false });
+    // if already liked, remove the like and add the dislike
+    if (voteState === VoteState.UP) {
+      setUpVotes(upVotes - 1);
+      setDownVotes(dislikeCount + 1);
+    }
   };
 
   return (
     <VoteButtonBase
       onVoteDown={handleDislike}
       onVoteUp={handleLike}
-      upVotes={likeCountState}
-      downVotes={dislikeCountState}
+      upVotes={upVotes}
+      downVotes={downVotes}
       voteState={voteState}
     />
   );
