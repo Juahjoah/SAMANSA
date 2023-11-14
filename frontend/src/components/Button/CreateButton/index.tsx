4667e9748780bff3d8b1f26da1384c3a7957643b
joung <15gpfk@naver.com>
2023-11-14 13:36:35 +0900

    feat: 모달 수정

--- frontend/src/components/Button/CreateButton/index.tsx
+++ frontend/src/components/Button/CreateButton/index.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import styles from './CreateButton.module.css';
 
 // api 요청 함수
--- frontend/src/components/Modal/index.tsx
+++ frontend/src/components/Modal/index.tsx
@@ -11,7 +11,7 @@ type ModalProps = {
   visible: boolean;
   variant?: string;
   onClose: () => void;
-  action: () => void;
+  action?: () => void;
 };
 
 export default function Modal({
@@ -24,17 +24,28 @@ export default function Modal({
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [secondcontent, setSecondContent] = useState('');
+  // 버튼 상태-> 확인버튼(false) or 취소+확인 버튼(true)
+  const [switchButton, setSwitchButton] = useState(false);
 
   useEffect(() => {
     if (variant === 'report') {
       setTitle('신고 하시겠습니까?');
       setContent('누적된 신고가 많은 단어는');
       setSecondContent('삭제될 수 있으니 주의해주세요.');
+      setSwitchButton(true);
     } else if (variant === 'delete') {
       setTitle('단어를 삭제하시겠습니까?');
       setContent('삭제된 단어는 다시 복구할 수 없으니');
       setSecondContent('신중히 결정해주세요.');
+      setSwitchButton(true);
+    } else if (variant === 'create') {
+      setTitle('로그인 후 이용하실수있습니다.');
+      setContent('다시 로그인한 후에 이용해주세요.');
+    } else if (variant === 'login') {
+      setTitle('닉네임이 비어있습니다.');
+      setContent('다시 닉네임을 입력해주세요.');
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const onMaskClick = (e: React.MouseEvent) => {
     if (e.target === e.currentTarget) {
@@ -49,8 +60,11 @@ export default function Modal({
   };
 
   const active = () => {
+    if (action) {
+      action();
+      console.log('asdf');
+    }
     onClose();
-    action();
   };
 
   return (
@@ -63,16 +77,19 @@ export default function Modal({
         onClick={maskClosable ? onMaskClick : undefined}
       >
         <div className={styles.modalInner}>
-          <Image src={alert} alt="alert" width={45} height={45} />
+          <Image src={alert} alt="alert" width={55} height={55} />
           <p>{title}</p>
           <p>
             {content} <br /> {secondcontent}
           </p>
           <div className={styles.container}>
-            <div className={styles.closebutton} onClick={close}>
-              취소
-            </div>
-            <div className={styles.deletebutton} onClick={active}>
+            {switchButton && (
+              <div className={styles.closebutton} onClick={close}>
+                취소
+              </div>
+            )}
+
+            <div className={styles.checkbutton} onClick={active}>
               확인
             </div>
           </div>
