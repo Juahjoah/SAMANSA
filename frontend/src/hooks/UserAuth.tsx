9633355f296feba68a82d1ffa35509fe8f5582e3
김주아 <07juahlily21@naver.com>
2023-11-14 14:29:27 +0900

    feat: access token 재발급 코드 추가 ( #35 )

--- frontend/src/hooks/UserAuth.tsx
+++ frontend/src/hooks/UserAuth.tsx
@@ -1,52 +1,58 @@
-import { deleteCookie, getCookie, setCookie } from './UserCookies';
+import { getCookie, setCookie, deleteCookie } from '@/hooks/UserCookies';
 
-const ACCESS_EXPIRY_TIME = 36000000;
-
-const SAMANSA_REFRESH_TOKEN = 'samansaRefreshToken';
-const SAMANSA_ACCESS_EXPIRY_TIME = 'samansaAccessExpiryTime';
-
+const ACCESS_EXPIRY_TIME = 3 * 60 * 60 * 1000;
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const accessToken = getCookie('accessToken');
 
-function checkExpireTime(expireTime: number): boolean {
-  return expireTime - new Date().getTime() < 30000;
-}
-
-export async function refreshTokenCheck() {
-  const expiryTime = Number(sessionStorage.getItem(SAMANSA_ACCESS_EXPIRY_TIME));
-
-  if (checkExpireTime(expiryTime)) {
-    try {
-      const response = await fetch(`${BASE_URL}/token/new`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          refreshToken: getCookie(SAMANSA_REFRESH_TOKEN),
-        }),
-      });
-
+function handleExpiration() {
+  fetch(`${BASE_URL}/member`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+    .then((response) => {
       if (response.ok) {
-        const responseData = await response.json();
-        const createdTime = new Date().getTime();
-        const newAccessToken = responseData.data.accessToken;
-
-        sessionStorage.setItem('accessToken', newAccessToken);
-        sessionStorage.setItem(
-          SAMANSA_ACCESS_EXPIRY_TIME,
-          (createdTime + ACCESS_EXPIRY_TIME).toString(),
-        );
-      } else {
-        throw new Error('Network response was not ok.');
+        deleteCookie('accessToken');
+        if (typeof window !== 'undefined') {
+          window.location.reload();
+        }
+        return response.json();
       }
-    } catch (error) {
-      console.error('error', error);
-    }
-  }
+    })
+    .catch((error) => {
+      console.error('실패:', error);
+      // 갱신 실패 시 처리 로직 추가
+    });
 }
 
-export function tokenError() {
-  deleteCookie(SAMANSA_REFRESH_TOKEN);
-  alert('로그인이 만료되었습니다.');
-  window.location.href = '/';
+export async function handleToken() {
+  const response = await fetch(`${BASE_URL}/token/new`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (response.ok) {
+    const responseData = await response.json();
+    const newAccessToken = responseData.data.accessToken;
+    const createdTime = new Date().getTime();
+
+    // 갱신된 토큰과 만료 시간을 저장
+    setCookie('accessToken', newAccessToken, { path: '/', httpOnly: true });
+    sessionStorage.setItem(
+      'accessExpiryTime',
+      (createdTime + ACCESS_EXPIRY_TIME).toString(),
+    );
+  } else {
+    alert('로그인 시간이 만료되었습니다.');
+    console.error(
+      'Error refreshing token:',
+      response.status,
+      response.statusText,
+    );
+    // 갱신 실패 시 처리 로직 추가
+    handleExpiration();
+  }
 }
