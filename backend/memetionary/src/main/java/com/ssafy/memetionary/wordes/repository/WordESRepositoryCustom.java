00c0bc9b45484c816e4ba9b07ef53d52d1e4bca3
hwanhee <dup07168@naver.com>
2023-11-16 09:27:45 +0900

    refactor: 정렬 기준 여러개 요청 가능(#53)

--- backend/memetionary/src/main/java/com/ssafy/memetionary/wordes/repository/WordESRepositoryCustom.java
+++ backend/memetionary/src/main/java/com/ssafy/memetionary/wordes/repository/WordESRepositoryCustom.java
@@ -2,12 +2,14 @@ package com.ssafy.memetionary.wordes.repository;
 
 import com.ssafy.memetionary.wordes.document.QueryType;
 import com.ssafy.memetionary.wordes.document.SearchFieldType;
+import com.ssafy.memetionary.wordes.document.SortType;
 import com.ssafy.memetionary.wordes.document.WordES;
 import com.ssafy.memetionary.wordes.document.WordESRequestType;
 import com.ssafy.memetionary.wordes.dto.WordESAutoCompleteResponse;
 
 import com.ssafy.memetionary.wordes.dto.WordESSearchResponse;
 
+import java.util.List;
 import org.springframework.data.domain.Pageable;
 
 public interface WordESRepositoryCustom {
@@ -16,7 +18,7 @@ public interface WordESRepositoryCustom {
 
     public WordESAutoCompleteResponse getAutoCompleteWords(String word);
 
-    public WordESSearchResponse searchWords(QueryType queryType, SearchFieldType fieldType, String name, String clientIP, Pageable pageable);
+    public WordESSearchResponse searchWords(QueryType queryType, SearchFieldType fieldType, String name, List<SortType> sortTypeList, String clientIP, Pageable pageable);
 
     public WordESSearchResponse searchWordIndex(String name, Pageable pageable, String clientIP);
 
